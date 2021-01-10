ac5b9b6e8e97ec878ae14f6435e6e3fc7a7120d6
Robert Massaioli <rmassaioli@atlassian.com>
2021-01-10 13:03:14 +1100

    Running webpack locally to get assets to show up.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+   return (
+     <p>Hello world, this is cool</p>
+   )
+ }
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '(Development) JSON Schema Viewer'
+  })]
+};
