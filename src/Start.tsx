1252b35b59646a1bb4e049fe48dbbdbd84d86b35
Robert Massaioli <rmassaioli@atlassian.com>
2021-02-05 07:56:40 +1100

    Issue #17: Wrote a guide on how to live-edit the json schema.
    This is better than writing a custom copy-paste support. Especially
    since it allows for live editing.

--- src/Start.tsx
+++ src/Start.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import TextField from '@atlaskit/textfield';
 import Button from '@atlaskit/button';
+import { Markdown } from './markdown';
 
 export type StartProps = RouteComponentProps & {
 
@@ -13,9 +14,69 @@ export type StartState = {
   urlInput?: string;
 }
 
+const DevelopingSchemaInstructions = `
+## Developing a JSON Schema
+
+If you are busy writing or generating a JSON Schema and you want to get a live experience of viewing that schema using
+JSON Schema Viewer then you have a few options.
+
+### Using a locally running server
+
+JSON Schema Viewer will work so long as your tool You could just host the file on your own machine and edit it from there.
+To test this out:
+
+1. In a terminal, go to the directory on your local filesystem that contains your JSON Schema.
+1. In that directory on your local machine, type in:
+   \`\`\`
+   npx http-server -p 9876 --cors -c-1
+   \`\`\`
+1. Navigate to [http://localhost:9876/][1] and click on your JSON Schema link.
+1. Copy the URL for the JSON Schema and paste it into the input above.
+
+And there you have it! You can now view your JSON Schema here. Using this method, you can also live edit the file
+on your local computer and then you can merely refresh JSON Schema Viewer in your browser and your new content will
+appear live.
+
+### Using Bitbucket Snippets
+
+To use Bitbucket Snippets:
+
+1. Navigate to: [https://bitbucket.org/snippets/new][2]
+1. Paste your JSON Schema into the file text input and fill in the other fields.
+1. Select "Permissions" => "Public" and Create Snippet.
+1. Once the snippet has been created, click on the "Raw" button and copy the raw link to your file.
+1. Paste that link into the input on this page.
+
+And there you have it! You can now view your JSON Schema here. If you wish to update it, merely go back to your gist,
+update the contents, re-copy the raw url (because it will have updated) and load it into JSON Schema viewer again.
+
+### Using Github Gists
+
+To use Github Gists:
+
+1. Navigate to: [https://gist.github.com/][3]
+1. Paste your JSON Schema into the file text input and fill in the other fields.
+1. Select "Create public gist" so that the gist can be viewed by anybody.
+1. Once the gist has been created, click on the "Raw" button and copy the raw link to your file.
+1. Paste that link into the input on this page.
+
+And there you have it! You can now view your JSON Schema here. If you wish to update it, merely go back to your gist,
+update the contents, re-copy the raw url (because it will have updated) and load it into JSON Schema viewer again.
+
+ [1]: http://localhost:9876/
+ [2]: https://bitbucket.org/snippets/new
+ [3]: https://gist.github.com/
+`.trim();
+
 export class StartWR extends React.PureComponent<StartProps, StartState> {
   private static InputWidth = styled.div`
     min-width: 600px;
+  `;
+
+  private static Guide = styled.div`
+    max-width: 600px;
+    text-align: left;
+    margin-top: 100px;
   `;
 
   state: StartState = {
@@ -43,6 +104,7 @@ export class StartWR extends React.PureComponent<StartProps, StartState> {
           <StartWR.InputWidth>
             <TextField isCompact={false} value={this.state.urlInput || ''} onChange={onTextChange} />
             <Button label="submit" onClick={handleOnClick} appearance="primary">Load Schema</Button>
+            <StartWR.Guide><Markdown source={DevelopingSchemaInstructions} /></StartWR.Guide>
           </StartWR.InputWidth>
         )}
       />
