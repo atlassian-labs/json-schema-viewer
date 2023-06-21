e46ddf9a1d1456f924ef6b52e233b41ca8638d8c
Sam Leatherdale <sleatherdale@atlassian.com>
2023-06-21 14:10:46 +1000

    EIWFY23Q4-1 Update sharp and Webpack due to install issues

--- src/SchemaEditor.tsx
+++ src/SchemaEditor.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { JsonSchema } from './schema';
 import Editor, { OnValidate, useMonaco } from '@monaco-editor/react';
 import type { IRange } from 'monaco-editor';
-import { ScrollType } from "./monaco-helpers";
+import { ScrollType } from './monaco-helpers';
 
 export type SchemaEditorProps = {
   initialContent: unknown;
@@ -22,39 +22,40 @@ const editorPreamble = `
 
 export const SchemaEditor: React.FC<SchemaEditorProps> = (props) => {
   const { initialContent, schema, validationRange, onValidate } = props;
-   const monaco = useMonaco();
+  const monaco = useMonaco();
 
-   useEffect(() => {
-      monaco?.languages.json.jsonDefaults.setDiagnosticsOptions({
-         validate: true,
-         allowComments: true,
-         schemas: [{
-            uri: "https://json-schema.app/example.json", // id of the first schema
-            fileMatch: ['a://b/example.json'],
-            schema: schema
-         }]
-     });
-   }, [monaco, schema]);
-   useEffect(() => {
-     if (!validationRange || !monaco) {
-       return;
-     }
-      monaco.editor.getEditors()
-        .forEach((codeEditor) => {
-          codeEditor.setSelection(validationRange)
-          codeEditor.revealRangeAtTop(validationRange, ScrollType.Smooth)
-        });
-   }, [monaco, validationRange])
+  useEffect(() => {
+    monaco?.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      allowComments: true,
+      schemas: [
+        {
+          uri: 'https://json-schema.app/example.json', // id of the first schema
+          fileMatch: ['a://b/example.json'],
+          schema: schema,
+        },
+      ],
+    });
+  }, [monaco, schema]);
+  useEffect(() => {
+    if (!validationRange || !monaco) {
+      return;
+    }
+    monaco.editor.getEditors().forEach((codeEditor) => {
+      codeEditor.setSelection(validationRange);
+      codeEditor.revealRangeAtTop(validationRange, ScrollType.Smooth);
+    });
+  }, [monaco, validationRange]);
 
-   return (
-      <Editor
-         height="97vh"
-         defaultLanguage="json"
-         value={editorPreamble + '\n' + JSON.stringify(initialContent, null, 2)}
-         path="a://b/example.json"
-         theme="vs-dark"
-         saveViewState={false}
-         onValidate={onValidate}
-      />
-   );
+  return (
+    <Editor
+      height="97vh"
+      defaultLanguage="json"
+      value={editorPreamble + '\n' + JSON.stringify(initialContent, null, 2)}
+      path="a://b/example.json"
+      theme="vs-dark"
+      saveViewState={false}
+      onValidate={onValidate}
+    />
+  );
 };
